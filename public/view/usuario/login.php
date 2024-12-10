b61860cd067c586fae0e19d553afd52c0d9b4bec
Leandro Oliveira <leandrocandido2222@gmail.com>
2024-12-10 08:52:22 -0400

    upp

--- public/view/usuario/login.php
+++ public/view/usuario/login.php
@@ -31,6 +31,7 @@ if($usuario){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrar</title>
     <link rel="stylesheet" href="../../css/Login.css">
+    <link rel="stylesheet" href="../../css/navbar.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
@@ -64,6 +65,9 @@ if($usuario){
         <div class="image-container">
             <img src="../../assets/img/Group 4.png" alt="img-login">
         </div>
-        <?php include('../Tweeb/Includes/navbar.php'); ?>
+        <?php
+            include("./Includes/navbar.php");
+            echo "Navbar incluída com sucesso!";
+        ?>
 </body>
 </html>
