fa46e7489250ba52332be7c7ec133b42ba1946a2
João Almeida <toxicyszconta@gmail.com>
2025-02-04 10:17:18 -0400

    coisas

--- public/js/js_task19.js
+++ public/js/js_task19.js
@@ -1,6 +1,23 @@
 // Seleciona todas as divisões de departamento
 const departments = document.querySelectorAll('.department');
 
+const listItems = document.querySelectorAll('.do-seu-jeito-ul-components li');
+
+
+listItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede o comportamento padrão do link
+        
+        // Remover a classe 'active' de todos os itens
+        listItems.forEach(i => i.classList.remove('active'));
+        
+        // Adicionar a classe 'active' ao item clicado
+        item.classList.add('active');
+        
+        console.log(item);
+    });
+});
+
 // Para cada departamento, adiciona um evento de clique
 departments.forEach(department => {
     department.addEventListener('click', (event) => {
